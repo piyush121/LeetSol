bf29b46c1bd3a073976650c026187ee820c37706
Piyush Chaudhary <pc1905@nyu.edu>
2016-06-22 21:36:30 -0400

    Not optimal.
    I believe it can still be optimized further by creating a 1D array. Although space complexity will increase but time complexity will be less.Moreover,  Most of the time you are not allowed to modify input as well.

--- src/DP/MinPathSum.cpp
+++ src/DP/MinPathSum.cpp
@@ -3,24 +3,17 @@ Solution for : https://leetcode.com/problems/minimum-path-sum/
 */
 
 int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size();
-        if (!m)
-            return 0;
-        int n = grid[0].size();
-        
-        for ( int i = 0; i < m ; i++) {
-            for (int j = 0; j <n; j++) {
-                int minCost = 0;
-                if (!i && j) {
-                    minCost = grid[i][j-1];
-                } else if (i && !j) {
-                    minCost = grid[i-1][j];
-                } else if (i && j) {
-                    minCost = std::min(grid[i][j-1], grid[i-1][j]);
-                }
-                grid[i][j] += minCost;
-            }
-        }
-        
-        return grid[m-1][n-1];
+       int minPathSum(vector<vector<int> > &grid) {
+        if (grid.empty() || grid[0].empty()) return 0;
+        int m = grid.size(), n = grid[0].size();
+
+        vector<int> dp(n + 1, INT_MAX);
+        dp[1] = 0;
+
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                dp[j + 1] = min(dp[j + 1], dp[j]) + grid[i][j];
+
+        return dp.back();
     }
+}
